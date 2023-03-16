246a3759a4cb22629b0eebef2756b3dc5abd1fd7
Alex Singleton <alex.singleton@keyin.com>
2023-03-16 13:43:25 -0230

    third commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,46 @@
 // i want to make a website to practice handling data
-function loadAPT(){
+function loadAPT() {
   let url = "https://api.github.com/users";
-  let response = fetch(url).then(
-   (response) => response.json()
- ).then(
-   (users) => {
-    users.forEach((user => {
-      console.log(user)
-      var output = `<figure><h3 id="login">${user.login}</h3><img src= "${user.avatar_url}"></figure>`;
-      document.querySelector("#profiles").innerHTML += output+"<br>";
-    }))
-  });
-};
-
-function chuckNorrisJoke(){
-   let url = "https://api.chucknorris.io/jokes/random";
-   fetch(url).then(
-    (response) => response.json()
-  ).then(
-    (joke) => {
-        console.log(joke.value+"<br>");
+  fetch(url)
+    .then((response) => response.json())
+    .then((users) => {
+      users.forEach((user) => {
+        let output = `<div  class="profile"><img src= "${user.avatar_url}"><h3 id="login">${user.login}</h3><br><ul id="details"><li>id: ${user.id}</li><li>page: <a href="${user.html_url}">${user.html_url}</a></li></ul></div>`;
+        document.querySelector("#profiles").innerHTML += output;
+      });
     });
-};
+}
 
-//  chuck norris joke
-let chuck = document.querySelector("#chuckNorris");
-chuck.addEventListener("click",chuckNorrisJoke);
+function idSearch(value) {
+  let url = "https://api.github.com/users";
+  fetch(url)
+    .then((response) => response.json())
+    .then((users) => {
+      users.forEach((user)=>{
+        if (value == user.id) {
+          let output = `<div  class="profile"><img src= "${user.avatar_url}"><h3 id="login">${user.login}</h3><br><ul id="details"><li>id: ${user.id}</li><li>page: <a href="${user.html_url}">${user.html_url}</a></li></ul></div>`;
+        };
+      });
+      document.querySelector("#searched").innerHTML = output;;
+    });
+  };
 
+async function awaitComment() {
+  let comment = await fetch(`https://jsonplaceholder.typicode.com/comments`)
+}
+
+///////////////////////////////////////////////////
 window.onload = () => {
   loadAPT();
-}
+};
+
+// search button
+const searchBttn = document.querySelector("#search");
+searchBttn.addEventListener("click", () => {
+  let value = document.querySelector("#id").value;
+  document.querySelector("#id").value = "";
+  idSearch(value);
+});
+
+// chuck button 
+const chuckBttn = document.querySelector("#chuckIT")
